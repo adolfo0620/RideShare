47b006a8d213333c8bae1cbf4101d43564d364a4
Adolfo Reyes <adolfo0620@gmail.com>
2018-01-18 19:44:01 -0500

    init project

---
+++ RideShare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RideShare
+//
+//  Created by adolfo reyes on 1/18/18.
+//  Copyright © 2018 adolfo reyes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
