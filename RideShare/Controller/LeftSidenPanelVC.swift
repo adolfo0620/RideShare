202a1b15fc7595f00b02c1e7e6a3a49e808b6ab2
Adolfo Reyes <adolfo0620@gmail.com>
2018-02-01 21:06:19 -0500

    creating login and signup page

--- RideShare/Controller/LeftSidenPanelVC.swift
+++ RideShare/Controller/LeftSidenPanelVC.swift
@@ -21,4 +21,9 @@ class LeftSidenPanelVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func signUpLoginBtnWasPressed(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let loginVC = storyboard.instantiateViewController(withIdentifier: "LoginVC") as? LoginVC
+        present(loginVC!, animated: true, completion: nil)
+    }
 }
